7b053d31e700c2c241aafd1934a87d1f98d01b5e
rajbharath <rbrajbharath1@gmail.com>
2014-08-13 22:45:27 +0530

    Refactor 1 - ConferenceRoom
    Make Booking History as inner class. added TO DO

--- src/main/ConferenceRoom.java
+++ src/main/ConferenceRoom.java
@@ -15,6 +15,24 @@ public class ConferenceRoom {
 	}
 
 	public boolean isAvailableFor(Event event) {
+		//TO DO find whether the room is available for this event or not
+		return false;
+	}
+}
+
+class BookingHistory {
+	List<Event> bookedEvents;
+
+	public void book(Event event) {
+		bookedEvents.add(event);
+	}
+
+	public void cancelBooking(Event event) {
+		bookedEvents.remove(event);
+	}
+
+	public boolean isAnyOverLappingEventFor(Event inputEvent) {
+		//TO DO find whether booked events has any event in the time of inputevent
 		return false;
 	}
 }
